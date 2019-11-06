06a62808e3a3cb39265829e1bf0eb96793dc7985
Lidrestoni <guilherme.antunes@uffs.edu.br>
2019-11-05 21:23:38 -0300

    Added testes on distance 558 payload 25

--- constants.h
+++ constants.h
@@ -1,11 +1,11 @@
-const int nPackets = 5 ;
+const int nPackets = 500 ;
 const int minTxPower = 2 ;
 const int maxTxPower = 17 ;
 const int minSf = 7 ;
 const int maxSf = 12 ;
 const int initTxPower = 2 ;
-const int initSf = 11 ;
-const int initIndexValidMessageArray = 1 ;
+const int initSf = 7 ;
+const int initIndexValidMessageArray = 2 ;
 const int validMessageArraySize = 6 ;
 const int validMessageArray[validMessageArraySize] = {76,76,25,5,150,230} ;
 const String originalMessage = "Dori me interimo; Adapare; Dori me; Ameno(2x); Latire; Latiremo; Dorime(...)" ;
